dea45c6d540899df9e4d9aa98e5e37369461b618
Shaulaev <mediggggh@gmai.com>
2022-11-06 03:24:35 +0300

    Added axios and loader

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { Post } from './src/Post';
-import { View, FlatList } from "react-native"
+import { View, Text, FlatList, Alert, ActivityIndicator } from "react-native"
+import axios from 'axios';
 
 export default function App() {
-  
+
+  const [isLoading, setIsLoading] = useState(true)
   const [value, setValue] = useState(false)
 
-  const data = useEffect(() => {
-    fetch("https://6366ee2679b0914b75d94e74.mockapi.io/articles/articles").then(data => data.json()).then(articles => setValue(articles))
-  });
+  const fetchArticles = () => {
+    setIsLoading(true)
+    axios
+      .get("https://6366ee2679b0914b75d94e74.mockapi.io/articles/articles")
+      .then(({data}) => setValue(data))
+        .catch((err) => {
+          Alert.alert("Произошла ошибка при загрузке статей")
+        }).finally(setIsLoading)
+  }
+
+  useEffect(fetchArticles, []);
+
+  if(isLoading) {
+    return  (
+        <View style={{flex: 1, justifyContent: "center", alignItems: 'center'}}>
+          <ActivityIndicator /> 
+          <Text>Загрузка...</Text>
+        </View>
+      )
+  }
 
   return (
     <View>
